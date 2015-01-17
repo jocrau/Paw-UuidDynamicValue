7c8a96d94b7d6df0acbfaf3bd92aea011c6f6b50
Jochen Rau <jocrau@kirk-2.local>
2015-01-17 05:52:58 -0500

    Add comment about code origin

--- UuidDynamicValue.js
+++ UuidDynamicValue.js
@@ -1,5 +1,7 @@
 var UuidDynamicValue = function() {
     this.evaluate = function(context) {
+		// See answer by http://stackoverflow.com/users/508537/briguy37
+		// for http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
   	  var d = new Date().getTime();
       var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           var r = (d + Math.random()*16)%16 | 0;
