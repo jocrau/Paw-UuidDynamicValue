33ad23829b3f46151a290e7de1414ad98b32e3af
Jochen Rau <jocrau@kirk-2.local>
2015-01-16 20:01:57 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+#UUID Dynamic Value (Paw Extension)
+
+This a Paw Extension that generates a UUID.
+
+##License
+
+This Paw Extension is released under the [MIT License](LICENSE). Feel free to fork, and modify!
---
+++ UuidDynamicValue.js
@@ -0,0 +1,17 @@
+var UuidDynamicValue = function() {
+    this.evaluate = function(context) {
+  	  var d = new Date().getTime();
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = (d + Math.random()*16)%16 | 0;
+          d = Math.floor(d/16);
+          return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+      });
+      return uuid;
+    }
+}
+
+UuidDynamicValue.identifier = "com.functional-thinking.UuidDynamicValue";
+
+UuidDynamicValue.title = "UUID Dynamic Value";
+
+registerDynamicValueClass(UuidDynamicValue);
